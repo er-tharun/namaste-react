69666e81d4b22afec4d2f51c95b226d520fb106f
Tharun <er.tharunravi@gmail.com>
2024-09-24 08:30:27 +0530

    Episode - 01

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", {id:"heading", abc:"xyz"}, "Hello World from React JS");
+
+console.log(heading); //object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
